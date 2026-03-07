75cebe9d4d27d2ae86d245c6434bac2fc251dded
innovasys56-prog <innovasys56@gmail.com>
2026-03-07 05:58:19 -0600

    Add files via upload

---
+++ Prueba2/conexion.js
@@ -0,0 +1,43 @@
+// conexion.js - Versión con CDN
+// Importar desde la CDN de Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  getDocs, 
+  query, 
+  where,
+  updateDoc,
+  doc,
+  onSnapshot,
+  orderBy 
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+// IMPORTANTE: Reemplaza con los datos de tu proyecto (Paso 2)
+const firebaseConfig = {
+    apiKey: "AIzaSyBpuWKh56n1HfqLpytVD7rXnetrhvswoHE",
+    authDomain: "portal-utc.firebaseapp.com",
+    projectId: "portal-utc",
+    storageBucket: "portal-utc.firebasestorage.app",
+    messagingSenderId: "1004079523692",
+    appId: "1:1004079523692:web:77860a1be7b871a7db7549",
+    measurementId: "G-V42WBP78BP"
+  };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { 
+  db, 
+  collection, 
+  addDoc, 
+  getDocs, 
+  query, 
+  where,
+  updateDoc,
+  doc,
+  onSnapshot,
+  orderBy 
+};
